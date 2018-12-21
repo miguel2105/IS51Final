ee8e4ba388fef663d04e3075045e6eefd1711f65
Miguel <miguel2105@mail.fresnostate.edu>
2018-12-20 19:46:47 -0800

    Final

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -3,6 +3,16 @@ import { Http } from '@angular/http';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
+
+
+
+export interface IOrder {
+  id: number;
+  image: string;
+  description: string;
+  quantity: number;
+}
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -10,6 +20,7 @@ import { ToastService } from '../toast/toast.service';
 })
 export class CartComponent implements OnInit {
 
+inventory: Array<IOrder> = [];
   constructor(
     private http: Http,
     private activatedRoute: ActivatedRoute,
@@ -18,6 +29,16 @@ export class CartComponent implements OnInit {
   ) { }
 
   async ngOnInit() {
+    const savedInventory = JSON.parse(localStorage.getItem('inventory'));
+    if (savedInventory && savedInventory.length > 0) {
+      this.inventory = savedInventory;
+    } 
+    }
   }
 
-}
+
+  
+
+
+
+
